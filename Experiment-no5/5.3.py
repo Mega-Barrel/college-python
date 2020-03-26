1fd2294c9c61ca0ac2ab12defd9b8aeeaf819897
Mega-Barrel <saurabhjoshi24business@gmail.com>
2020-03-26 21:45:51 +0530

    added code for sql using python

---
+++ Experiment-no5/5.3.py
@@ -0,0 +1,13 @@
+# Python code for implementing CURD operation using SQL
+
+import mysql.connector
+mydb = mysql.connector.connect(host="localhost", user="root", passwd = "Saurabh240301", database = "test")
+
+mycursor = mydb.cursor()
+
+mycursor.execute('select * from student')
+
+result = mycursor.fetchall()
+
+for i in result:
+    print(i)
