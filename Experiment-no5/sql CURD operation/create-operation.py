4e386184347019559b34883949ee379614054574
Mega-Barrel <saurabhjoshi24business@gmail.com>
2020-03-27 20:09:04 +0530

    added code for creating database using python.

---
+++ Experiment-no5/sql CURD operation/create-operation.py
@@ -0,0 +1,20 @@
+# Write a python program to implement CURD operation in sql.
+
+'''
+CURD stand for 
+1. Create
+2. Update
+3. Read
+4. Delete
+'''
+
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", passwd = "Saurabh240301", database = "customer")
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customer_info(customer_id INT AUTO_INCREMENT PRIMARY KEY, customer_name VARCHAR(50), customer_address VARCHAR(50))")
+
+mydb.commit()
+
