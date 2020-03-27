9ebddcb54a5e64951fa4b59a822c29cf532c8af7
Mega-Barrel <saurabhjoshi24business@gmail.com>
2020-03-27 21:14:55 +0530

    inserted 6 data into table

---
+++ Experiment-no5/sql CURD operation/insertion-operation.py
@@ -0,0 +1,27 @@
+# This code is for adding data into a table.
+
+import mysql.connector
+
+def insertDataIntoTable(customer_id, customer_name, customer_address):
+    
+    mydb = mysql.connector.connect(host="localhost", user="root", passwd = "Saurabh240301", database = "customer")
+
+    mycursor = mydb.cursor()
+
+    mySql_insert_query = """INSERT INTO customer_information(customer_id, customer_name, customer_address) VALUES (%s, %s, %s)"""
+
+    recordTuple = (customer_id, customer_name, customer_address)
+    mycursor.execute(mySql_insert_query, recordTuple)
+
+    mydb.commit()
+    mycursor.close()
+    mydb.close()
+    # print('Data inserted successfully')
+
+insertDataIntoTable(1, 'Saurabh', 'Vasai')
+insertDataIntoTable(2, 'Nilesh', 'Borivali')
+insertDataIntoTable(3, 'Atharva', 'Andheri')
+insertDataIntoTable(4, 'Chaitanya', 'Dadar')
+insertDataIntoTable(5, 'Zack', 'Churchgate')
+insertDataIntoTable(6, 'Sam', 'Bandra')
+
